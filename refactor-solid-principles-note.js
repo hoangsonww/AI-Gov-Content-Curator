b58c25be15a217d38d48ccd483301c94b413c899
dav nguyxn <hoangson091104@gmail.com>
2026-03-01 08:55:10 -0500

    chore: add SOLID refactor note helper

---
+++ refactor-solid-principles-note.js
@@ -0,0 +1,7 @@
+const repoName = 'AI-Gov-Content-Curator';
+
+function describeSolidRefactorNote() {
+  return `${repoName}: prefer small modules with one clear responsibility.`;
+}
+
+module.exports = { describeSolidRefactorNote };
