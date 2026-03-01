67744e0682fb23abfd7c3a665b891b746369ceeb
dav nguyxn <hoangson091104@gmail.com>
2026-03-01 08:59:31 -0500

    Delete refactor-solid-principles-note.js
    Signed-off-by: dav nguyxn <hoangson091104@gmail.com>

--- refactor-solid-principles-note.js
+++
@@ -1,7 +0,0 @@
-const repoName = 'AI-Gov-Content-Curator';
-
-function describeSolidRefactorNote() {
-  return `${repoName}: prefer small modules with one clear responsibility.`;
-}
-
-module.exports = { describeSolidRefactorNote };
