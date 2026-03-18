366b45533a63fa6dbc3a78154ed06dda147d3b70
Son Nguyen <hoangson091104@gmail.com>
2026-03-18 10:38:34 -0400

    chore: fix install flow and normalize line-ending config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-  dest: 'public',
+const withPWA = require("next-pwa")({
+  dest: "public",
   register: true,
   skipWaiting: true,
   disable: false,
